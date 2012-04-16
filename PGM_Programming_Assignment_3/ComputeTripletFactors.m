f3bb5ffedb0b491f2639dc83ca02878ea7868baf
Tom Dong <tom.tung.dyb@gmail.com>
2012-04-17 03:57:20 +0800

    Completed assignment 3.

---
+++ PGM_Programming_Assignment_3/ComputeTripletFactors.m
@@ -0,0 +1,43 @@
+function factors = ComputeTripletFactors (images, tripletList, K)
+% This function computes the triplet factor values for one word.
+%
+% Input:
+%   images: An array of structs containing the 'img' value for each
+%     character in the word.
+%   tripletList: An array of the character triplets we will consider (other
+%     factor values should be 1). tripletList(i).chars gives character
+%     assignment, and triplistList(i).factorVal gives the value for that
+%     entry in the factor table.
+%   K: The alphabet size (accessible in imageModel.K for the provided
+%     imageModel).
+%
+% Hint: Every character triple in the word will use the same 'val' table.
+%   Consider computing that array once and then resusing for each factor.
+%
+% Copyright (C) Daphne Koller, Stanford University, 2012
+
+
+n = length(images);
+
+% If the word has fewer than three characters, then return an empty list.
+if (n < 3)
+    factors = [];
+    return
+end
+
+factors = repmat(struct('var', [], 'card', [], 'val', []), n - 2, 1);
+
+% Your code here:
+val = ones(1, K*K*K);
+for i = 1:length(tripletList)
+    chars = tripletList(i).chars;
+    val(chars(1) + (chars(2)-1)*K + (chars(3)-1)*K^2) = tripletList(i).factorVal;
+end
+
+for i = 1:n-2
+    factors(i).var = [i i+1 i+2];
+    factors(i).card = [K K K];    
+    factors(i).val = val;
+end
+
+end
